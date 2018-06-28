b99b2dd56256d361e51fee4e35b84868dc05831d
Alveera Khan <alveera@fb.com>
2018-06-28 14:58:32 -0700

    Added details activity

--- app/src/main/java/com/codepath/alveera/flicks/models/Movie.java
+++ app/src/main/java/com/codepath/alveera/flicks/models/Movie.java
@@ -2,13 +2,19 @@ package com.codepath.alveera.flicks.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel // annotation indicates class is Parcelable
 public class Movie {
-    // values from API
-    private String title;
-    private String overview;
-    private String posterPath; // only the path
-    private String backdropPath;
+    // values from API must be public for parceler
+    public String title;
+    public String overview;
+    public String posterPath; // only the path
+    public String backdropPath;
+    public Double voteAverage; // average ratings from all users on a scale from 0 to 10 inclusive
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
 
     // initialize from JSON data
     public Movie(JSONObject object) throws JSONException{
@@ -16,6 +22,7 @@ public class Movie {
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
     }
 
     public String getTitle() {
@@ -32,6 +39,10 @@ public class Movie {
 
     public String getBackdropPath() {
         return backdropPath;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
 
